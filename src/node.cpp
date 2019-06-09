ab83953aaa052679ef0e1b892844f760d28fd0d8
YourBroDuke <CSscrewX@gmail.com>
2019-06-09 12:49:42 +0800

    need parser.y

--- src/node.cpp
+++ src/node.cpp
@@ -19,36 +19,38 @@ void FileNode::Visit(){
     }
 }
 
-PackageNode::PackageNode(QualifiedNameNode *node){
+PackageNode::PackageNode() 
+
+PackageNode::PackageNode(QualifiedNameNode *node) {
     this->qNode = node;
 }
 
-void PackageNode::Visit(){
+void PackageNode::Visit() {
     this->qNode->Visit();
 }
 
-ImportNode::ImportNode(){
+ImportNode::ImportNode() {
 
 }
 
-void ImportNode::Visit(){
+void ImportNode::Visit() {
 
 }
 
-IdentifierNode::IdentifierNode(const string& name){
+IdentifierNode::IdentifierNode(const string& name) {
     this->name = name;
 }
 
-void IdentifierNode::Visit(){
+void IdentifierNode::Visit() {
     cout << this->name << endl;
 }
 
-TypeDeclNode::TypeDeclNode(ClassOrInterface t, ClassDeclNode *node){
+TypeDeclNode::TypeDeclNode(ClassOrInterface t, ClassDeclNode *node) {
     this->type = t;
     this->classDecl = node;
 }
 
-void TypeDeclNode::Visit(){
+void TypeDeclNode::Visit() {
     if (this->type == Class){
         cout << "class" << endl;
     }else{
@@ -58,33 +60,230 @@ void TypeDeclNode::Visit(){
     this->classDecl->Visit();
 }
 
-ClassDeclNode::ClassDeclNode(const string& name, vector<ClassBodyDeclNode*> *decls){
-    this->classBodyDecls = decls;
+ClassDeclNode::ClassDeclNode(const string& name, ClassBodyNode *classBody) {
+    this->classBody = classBody;
     this->className = new IdentifierNode(name);
 }
 
-void ClassDeclNode::Visit(){
+void ClassDeclNode::Visit() {
     this->className->Visit();
+    this->classBody->Visit();
+}
 
-    for (auto node : *this->classBodyDecls){
+QualifiedNameNode::QualifiedNameNode() {
+    this->identifiers = new vector<IdentifierNode*>;
+}
+
+void QualifiedNameNode::Visit() {
+    for (auto node : *this->identifiers){
         node->Visit();
     }
 }
 
-ClassBodyDeclNode::ClassBodyDeclNode(){
+
+ClassBodyNode::ClassBodyNode() {
 
 }
 
-void ClassBodyDeclNode::Visit(){
-
-}
-
-QualifiedNameNode::QualifiedNameNode(){
-    this->identifiers = new vector<IdentifierNode*>;
-}
-
-void QualifiedNameNode::Visit(){
-    for (auto node : *this->identifiers){
+void ClassBodyNode::Visit() {
+    for (auto node : *this->memberDecls)
         node->Visit();
+}
+
+MemberDeclNode::MemberDeclNode(Node *decl) {
+    this->mainDecl = decl;
+}
+
+void MemberDeclNode::printType(ModifierType type) {
+    if (type == PUBLIC_TYPE) cout << "it has public attribute" << endl;
+    if (type == STATIC_TYPE) cout << "it has static attribute" << endl;
+}
+
+void MemberDeclNode::Visit() {
+    this->mainDecl->Visit();
+    for (auto crt : this->modifiers)
+        this->printType(crt);
+}
+
+MethodDeclNode::MethodDeclNode(TypeTypeNode *type, const string& name, BlockNode *block) {
+    this->typeInfo = type;
+    this->nodeName = new IdentifierNode(name);
+    this->methodBody = block;
+}
+
+void MethodDeclNode::Visit() {
+    this->typeInfo->Visit();
+    this->nodeName->Visit();
+    for (auto node : *this->params)
+        node->Visit();
+    this->methodBody->Visit();
+}
+
+TypeTypeNode::TypeTypeNode(PrimitiveTypeOrNot type) {
+    this->type = type;
+}
+
+void TypeTypeNode::printType() {
+    if (this->type == VOID_TYPE)
+        cout << "This is a void method" << endl;
+}
+
+void TypeTypeNode::Visit() {
+    this->printType();
+    for (auto node : *this->typeInfo)
+        node->visit();
+}
+
+FormalParamNode::FormalParamNode(TypeTYpeNode *type, VariableDeclaratorIDNode node) {
+    this->paramType = type;
+    this->declNode = node;
+}
+
+void FormalParamNode::Visit() {
+    this->paramType->Visit();
+    this->declNode->Visit();
+}
+
+VariableDeclaratorIDNode::VariableDeclaratorIDNode(int dim, const string& name) {
+    this->arrayDim = dim;
+    this->variableName = new IdentifierNode(name);
+}
+
+void VariableDeclaratorIDNode::Visit() {
+    cout << "it has " << this->arrayDim << "dims" << endl;
+    this->variableName->Visit();
+}
+
+BlockNode::BlockNode() {
+
+}
+
+void BlockNode::Visit() {
+    for (auto node : *this->stats) {
+        node->Visit();
+    }
+}
+
+BlockStatementNode::BlockStatementNode(StatementNode *stat) {
+    this->stat = stat;
+}
+
+void BlockStatementNode::Visit() {
+    this->stat->Visit();
+}
+
+StatementNode::StatementNode(StatementType type, Node *stat) {
+    this->type = type;
+    this->stat = stat;
+}
+
+void StatementNode::printType() {
+    if (this->type == EXPR_TYPE) {
+        cout << "this is a expr statement" << endl;
+    }
+}
+
+void StatementNode::Visit() {
+    this->printType();
+    this->stat->Visit();
+}
+
+ExprNode::ExprNode(ExprType type, PrimaryNode *node) {
+    this->type = type;
+    this->primary = node;
+    this->ids = NULL;
+    this->methodCallParams = NULL;
+    this->subExp1 = this->subExpr2 = NULL;
+}
+
+ExprNode::ExprNode(ExprType type) {
+    this->type = type;
+    this->primary = NULL;
+    this->ids = NULL;
+    this->methodCallParams = NULL;
+    this->subExp1 = this->subExpr2 = NULL;
+}
+
+ExprNode::ExprNode(ExprType type, ExprNode *node) {
+    this->type = type;
+    this->primary = NULL;
+    this->ids = NULL;
+    this->methodCallParams = NULL;
+    this->subExpr1 = node;
+    this->subExpr2 = NULL;
+}
+
+ExprNode::ExprNode(ExprType type, ExprNode *node1, ExprNode *node2) {
+    this->type = type;
+    this->primary = NULL;
+    this->ids = NULL;
+    this->methodCallParams = NULL;
+    this->subExpr1 = node1;
+    this->subExpr2 = node2;
+}
+
+void ExprNode::Visit() {
+    if (this->primary != NULL) this->primary->Visit();
+    if (this->ids != NULL) {
+        for (auto node : *this->ids)
+            node->Visit();
+    }
+    if (this->methodCallParams != NULL)
+        this->methodCallParams->Visit();
+}
+
+MethodCallParamsNode::MethodCallParamsNode() {
+
+}
+
+void MethodCallParamsNode::Visit() {
+    for (auto node : *this->exprs)
+        node->Visit();
+}
+
+PrimaryNode::PaimaryNode(PrimaryNodeType type, ExprNode *node) {
+    this->type = type;
+    this->expr = node;
+    this->literal = NULL;
+    this->id = NULL;
+}
+
+PrimaryNode::PrimaryNode(PrimaryNodeType type, LiteralNode *node) {
+    this->type = type;
+    this->expr = NULL;
+    this->literal = node;
+    this->id = NULL;
+}
+
+PrimaryNode::PrimaryNode(PrimaryNodeType type, const string& name) {
+    this->type = type;
+    this->expr = NULL;
+    this->literal = NULL;
+    this->id = new IdentifierNode(name);
+}
+
+void PrimaryNode::Visit() {
+    if (literal != NULL)
+        this->literal->Visit();
+}
+
+LiteralNode::LiteralNode(LiteralType type, int64_t val) {
+    this->type = type;
+    this->intVal = val;
+}
+
+LiteralNode::LiteralNode(LiteralNode type, double val) {
+    this->type = type;
+    this->floatVal = val;
+}
+
+LiteralNode::LiteralNode(LiteralNode type, const string& val) {
+    this->type = type;
+    this->stringVal = val;
+}
+
+void LiteralNode::Visit() {
+    if (this->type == STRING_LIT) {
+        cout << this->stringVal << endl;
     }
 }
